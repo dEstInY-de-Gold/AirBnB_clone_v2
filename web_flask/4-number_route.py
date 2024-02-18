d76cb193531ff4c6e047f89fef60ddc1894d3ffa
Noah <noahayebu@gmail.com>
2024-02-18 18:28:32 +0000

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -41,5 +41,13 @@ def pythonisfun(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''
+    display “n is a number” only if n is an integer
+    '''
+    return f'{n} is a number'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
